d09abcfdc687bc9cfb1c2a22984f795887fdaa64
Yuvaan8 <yuvaanbhandari007@gmail.com>
2026-06-01 17:10:44 +0530

    initial changes

---
+++ precedence.py
@@ -0,0 +1,14 @@
+v = 4
+w = 5
+x = 8
+y = 2
+z = 0
+z = (v + w)* x / y
+print('v + w * x / y', z)
+
+name = 'Alex'
+age = 0
+if name == 'Alex' or name == 'John' and age >= 10:
+    print('Hello!')
+else: 
+    print('Goodbye!')
