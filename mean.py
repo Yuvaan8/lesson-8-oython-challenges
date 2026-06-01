1f5ff6f09a9509d2b69e11d9a59a1717280e2e0b
Yuvaan8 <yuvaanbhandari007@gmail.com>
2026-06-01 17:26:54 +0530

    initial commit

---
+++ mean.py
@@ -0,0 +1,10 @@
+mean1 = 38
+wrong_number = 36
+correct_number = 56
+total_number = 40
+sum = mean1*total_number
+print('the sum of the 40 numbers is', sum)
+num2 = mean1 - wrong_number - correct_number
+print('sum - wrong_number - correct_number =', num2)
+mean2 = num2/total_number
+print(mean2)
